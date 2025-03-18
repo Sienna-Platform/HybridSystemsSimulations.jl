fa01dbbb0d8fde520b4a827ae4e6a33a75314f5b
rodrigomha <rodrigomha@gmail.com>
2025-03-18 10:44:43 -0700

    make decision models default

--- src/core/decision_models.jl
+++ src/core/decision_models.jl
@@ -1,4 +1,4 @@
-abstract type HybridDecisionProblem <: PSI.DecisionProblem end
+abstract type HybridDecisionProblem <: PSI.DefaultDecisionProblem end
 
 struct MerchantHybridEnergyCase <: HybridDecisionProblem end
 struct MerchantHybridEnergyFixedDA <: HybridDecisionProblem end
